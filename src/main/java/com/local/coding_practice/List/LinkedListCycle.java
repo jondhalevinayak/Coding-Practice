8832953c672b2a830d625f3601a0dd3497f916af
Vinayak Jondhale <vinayakjondhale@Vinayaks-MacBook-Pro.local>
2023-09-16 14:55:45 -0500

    first commit

---
+++ src/main/java/com/local/coding_practice/List/LinkedListCycle.java
@@ -0,0 +1,31 @@
+package com.local.coding_practice.List;
+
+public class LinkedListCycle {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        boolean result = hasCycle(head);
+        System.out.println("Result is :: "+result);
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
---
+++ src/main/java/com/local/coding_practice/List/ListNode.java
@@ -0,0 +1,13 @@
+package com.local.coding_practice.List;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+}
